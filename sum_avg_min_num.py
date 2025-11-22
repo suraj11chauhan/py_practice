cdafeddb552bfeebb513b554ec31ac6bd4913fb5
Suraj <112368828+suraj11chauhan@users.noreply.github.com>
2025-11-22 13:42:39 +0530

    Fix syntax errors in sum_avg_min_num.py

--- sum_avg_min_num.py
+++ sum_avg_min_num.py
@@ -1,10 +1,10 @@
 numbers = []
 while True:
     num = int(input('Enter the number(-999 to quit): '))
-    if num = -999
+    if num == -999 :
         break
     numbers.append(num)
-print(f"The Sum of numbers is: "sum(numbers))
-print(f"The Avg of numbers is: "sum(numbers)/len(numbers))
-print(f"The Max of numbers is: "max(numbers))
-print(f"The Min of numbers is: "min(numbers))
+print(f"The Sum of numbers is: {sum(numbers)}")
+print(f"The Avg of numbers is: {sum(numbers)/len(numbers)}")
+print(f"The Max of numbers is: {max(numbers)}")
+print(f"The Min of numbers is: {min(numbers)}")
